9753dafa5c7d62c5a2175dfe5aa73f73ba471fe4
drighetto <drighetto@excellium-services.com>
2021-03-03 13:35:30 +0100

    Refactor demo script

--- demo.sh
+++ demo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "=== Execute Demonstration ==="
+echo "[+] Runner properties"
+cat /proc/meminfo | head -1
+cat /proc/cpuinfo | grep "model name" | sort -u
 for script in test*.py
 do
     echo "[+] Execute '$script' ..."
