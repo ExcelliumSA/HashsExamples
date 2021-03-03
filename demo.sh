1bdaa36a637360d85dd7570a3129327b09a186ef
drighetto <drighetto@excellium-services.com>
2021-03-03 13:29:49 +0100

    Refactor demo script

--- demo.sh
+++ demo.sh
@@ -3,6 +3,6 @@ echo "=== Execute Demonstration ==="
 for script in test*.py
 do
     echo "[+] Execute '$script' ..."
-    $($script ./rockyou_1000.txt)
+    ./$script ./rockyou_1000.txt
 done
 exit 0
