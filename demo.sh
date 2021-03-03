8f4fe9fb02630e5eb35abb39e914f4729d8595e0
drighetto <drighetto@excellium-services.com>
2021-03-03 13:27:19 +0100

    Refactor demo script

--- demo.sh
+++ demo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "=== Execute Demonstration ==="
-for script in ./test*.py
+for script in test*.py
 do
-    echo "[+] $script: $($script ./rockyou_1000.txt)"
+    echo "[+] Execute '$script' ..."
+    $($script ./rockyou_1000.txt)
 done
 exit 0
